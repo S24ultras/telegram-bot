78b3caa5edf7e656afabfba9f393309d2c983337
Krish <nexus616@proton.me>
2025-08-18 16:05:22 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+API_ID = 27445790
+API_HASH = "9ba9db1e32788364afd9757ed5d7f1f5"
+BOT_TOKEN = "7893773198:AAGlrAZ9pRZrZon-_zraNF8e6hK1u55UQeY"
+
+CHANNEL_LINK = "https://t.me/+MD649XIeINgwYTQ1"
+CHANNEL_USERNAME = "AahaKalyanam"
+
+FOLDER_PATH = r"C:\Users\krish\Desktop\myfiles"
+
+bot = Client("sub_check_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@bot.on_message(filters.command("start"))
+async def start(client, message):
+    ...
+    # (rest of your code same as you gave me)
+    ...
+
+bot.run()
