a286a4a29efaa4c9bd9743bd3bc018f7cd3f661a
S24ultras <nexus616@proton.me>
2025-08-23 16:01:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ API_ID = 27445790
 API_HASH = "9ba9db1e32788364afd9757ed5d7f1f5"
 BOT_TOKEN = "7893773198:AAGlrAZ9pRZrZon-_zraNF8e6hK1u55UQeY"
 
-CHANNEL_LINK = "https://t.me/+MD649XIeINgwYTQ1"
-CHANNEL_USERNAME = "AahaKalyanam"
+CHANNEL_LINK = "https://t.me/+EdiH5HKvpyUyMzNl"
+CHANNEL_USERNAME = "Dolby"
 
 FOLDER_PATH = r"C:\Users\krish\Desktop\myfiles"
 
